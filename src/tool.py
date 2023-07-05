f38b66eda8610eda2d32eca8cb98d6264c4fa74b
cytobi <tobi81548@gmail.com>
2023-07-05 15:22:41 +0200

    remove icon from tool class

--- src/tool.py
+++ src/tool.py
@@ -16,7 +16,6 @@ class ToolFactory:
 class Tool(ABC):
     name = ""
     description = ""
-    icon = None
 
     def __init__(self, name, description, icon):
         self.name = name
@@ -37,7 +36,7 @@ class DragTool(Tool):
     drag_start_y = 0
 
     def __init__(self):
-        super().__init__("drag", "Drag Tool", None)
+        super().__init__("drag", "Drag Tool")
 
     def handle_node_press(self, window, node, event):
         debug("Starting drag of node " + str(node.name))
@@ -55,7 +54,7 @@ class DragTool(Tool):
 
 class SelectTool(Tool):
     def __init__(self):
-        super().__init__("select", "Select Tool", None)
+        super().__init__("select", "Select Tool")
 
     def handle_node_press(self, window, node, event):
         pass
